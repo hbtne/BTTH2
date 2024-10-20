ce6cd008226c1d0435a881304b8660bae98f501e
Bao Tran Ha <hbtran2004tnct@gmail.com>
2024-10-20 23:03:19 +0700

    clone theo video

---
+++ ThucHanhWebMVC/Repository/ILoaiSanPhamRepository.cs
@@ -0,0 +1,13 @@
+﻿using ThucHanhWebMVC.Models;
+namespace ThucHanhWebMVC.Repository
+{
+    public interface ILoaiSanPhamRepository
+    {
+        TLoaiSp Add(TLoaiSp loaiSp);
+        TLoaiSp Update(TLoaiSp loaiSp);
+        TLoaiSp Delete(TLoaiSp maloaiSp);
+        TLoaiSp GetLoaiSp(TLoaiSp maloaiSp);
+        IEnumerable<TLoaiSp> GetAllLoaiSP();
+
+    }
+}
---
+++ ThucHanhWebMVC/ViewComponents/LoaiSanPhamMenuViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using ThucHanhWebMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using ThucHanhWebMVC.Repository;
+
+namespace ThucHanhWebMVC.ViewComponents
+{
+    public class LoaiSanPhamMenuViewComponent : ViewComponent
+    {
+        private readonly ILoaiSanPhamRepository _loaiSanPham;
+        public LoaiSanPhamMenuViewComponent(ILoaiSanPhamRepository loaisanpham) 
+        {
+            _loaiSanPham = loaisanpham;        
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var loaisp = _loaiSanPham.GetAllLoaiSP().OrderBy(x => x.Loai);
+            return View(loaisp);
+        }
+    }
+}
